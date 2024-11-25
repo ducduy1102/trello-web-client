e323cfa330de4e2df446e7fb4fde3e9987f2b346
ducduy1102 <ducduy1110u@gmail.com>
2024-11-25 22:56:33 +0700

    Update config env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,17 +3,17 @@
 
 let apiRoot = "";
 // C1: Dùng import.meta.env của vite
-if (import.meta.env.MODE === "development" || import.meta.env.DEV) {
-  apiRoot = "http://localhost:8888";
-} else if (import.meta.env.BUILD_MODE === "production") {
-  apiRoot = "https://trello-web-server.onrender.com/";
-}
-
-// C2: Config lại process.env trong vite.config.js
-// if (process.env.BUILD_MODE === "dev") {
+// if (import.meta.env.MODE === "development" || import.meta.env.DEV) {
 //   apiRoot = "http://localhost:8888";
-// } else if (process.env.BUILD_MODE === "production") {
+// } else if (import.meta.env.BUILD_MODE === "production") {
 //   apiRoot = "https://trello-web-server.onrender.com/";
 // }
+
+// C2: Config lại process.env trong vite.config.js
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8888";
+} else if (process.env.BUILD_MODE === "production") {
+  apiRoot = "https://trello-web-server.onrender.com/";
+}
 
 export const API_ROOT = apiRoot;
