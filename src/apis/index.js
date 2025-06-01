24ea6d9aee02ef5a7f56dac2a3bb3cbbc9c1c927
ducduy1102 <ducduy1110u@gmail.com>
2025-06-01 18:00:40 +0700

    Feat: Invite User to Board by Email

--- src/apis/index.js
+++ src/apis/index.js
@@ -124,3 +124,12 @@ export const updateCardDetailsAPI = async (cardId, updateData) => {
   );
   return response.data;
 };
+
+export const inviteUserToBoardAPI = async (data) => {
+  const response = await authorizedAxiosInstance.post(
+    `${API_ROOT}/v1/invitations/board`,
+    data
+  );
+  toast.success("User invited to board successfully!");
+  return response.data;
+};
