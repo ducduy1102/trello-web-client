c24cd5a28f891f60777e046744df22b6d27419c9
ducduy1102 <ducduy1110u@gmail.com>
2025-06-06 00:02:08 +0700

    Hotfix: fix import createNewBoardAPI

--- src/pages/Boards/create.jsx
+++ src/pages/Boards/create.jsx
@@ -17,6 +17,7 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
 import { styled } from "@mui/material/styles";
+import { createNewBoardAPI } from "@/apis";
 const SidebarItem = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
