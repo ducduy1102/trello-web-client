67469f110dc75a74c73a08c495f508ffcff41875
ducduy1102 <ducduy1110u@gmail.com>
2025-06-12 18:39:47 +0700

    Feat: Input Auto Search Board

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -9,19 +9,14 @@ import Recent from "./Menus/Recent";
 import Started from "./Menus/Started";
 import Templates from "./Menus/Templates";
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Badge from "@mui/material/Badge";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import Tooltip from "@mui/material/Tooltip";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import Profiles from "./Menus/Profiles";
 import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
-import InputAdornment from "@mui/material/InputAdornment";
-import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Notifications from "./Notifications/Notifications";
+import AutoCompleteSearchBoard from "./SearchBoards/AutoCompleteSearchBoard";
 
 const AppBar = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -121,51 +116,8 @@ const AppBar = () => {
         }}
         gap={3}
       >
-        <TextField
-          id='outlined-search'
-          label='Search...'
-          type='text'
-          size='small'
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position='start'>
-                <SearchIcon sx={{ color: "white" }} />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment position='end'>
-                <CloseIcon
-                  fontSize='small'
-                  sx={{
-                    color: searchValue ? "white" : "transparent",
-                    cursor: "pointer",
-                  }}
-                  onClick={() => setSearchValue("")}
-                />
-              </InputAdornment>
-            ),
-          }}
-          sx={{
-            minWidth: "120px",
-            maxWidth: "180px",
-            "& label": { color: "white" },
-            "& input": { color: "white" },
-            "& label.Mui-focused": { color: "white" },
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                borderColor: "white",
-              },
-              "&:hover": {
-                borderColor: "white",
-              },
-              "&.Mui-focused fieldset": {
-                borderColor: "white",
-              },
-            },
-          }}
-        />
+        {/* Search Boarb */}
+        <AutoCompleteSearchBoard />
         <ModeSelect />
 
         {/* Dark - Light Mode */}
---
+++ src/components/AppBar/SearchBoards/AutoCompleteSearchBoard.jsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import CircularProgress from "@mui/material/CircularProgress";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+import { createSearchParams, useNavigate } from "react-router-dom";
+
+/**
+ * https://mui.com/material-ui/react-autocomplete/#asynchronous-requests
+ */
+function AutoCompleteSearchBoard() {
+  const navigate = useNavigate();
+
+  // State handles displaying fetched results from API
+  const [open, setOpen] = useState(false);
+  // State stores the list of fetched boards
+  const [boards, setBoards] = useState(null);
+  // Loading when start call api fetch boards
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // When closing the result list, clear the boards to null
+    if (!open) {
+      setBoards(null);
+    }
+  }, [open]);
+
+  // Process the data received from the input then call the API to get the result (need to add useDebounceFn as below)
+  const handleInputSearchChange = (event) => {
+    const searchValue = event.target?.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+
+    // Use react-router-dom's createSearchParams to create a standard searchPath with q[title] to call the API
+    const searchPath = `?${createSearchParams({ "q[title]": searchValue })}`;
+    console.log(searchPath);
+
+    // Call API...
+  };
+  // Do useDebounceFn...
+
+  // When we select a specific board, we will navigate to that board
+  const handleSelectedBoard = (event, selectedBoard) => {
+    // Check if a specific board is selected before calling navigate
+    console.log(selectedBoard);
+  };
+
+  return (
+    <Autocomplete
+      sx={{ width: 220 }}
+      id='asynchronous-search-board'
+      // This text appears when boards is null or after fetching boards but it is empty - no results
+      noOptionsText={!boards ? "Type to search board..." : "No board found!"}
+      // Handle opening and closing of search results
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      // getOptionLabel: let Autocomplete get the title of the board and display it
+      getOptionLabel={(board) => board.title}
+      options={boards || []}
+      isOptionEqualToValue={(option, value) => option._id === value._id}
+      loading={loading}
+      // onInputChange will run when typing content into the input tag
+      onInputChange={handleInputSearchChange}
+      // onChange of the entire Autocomplete will run when we select a result (here is board)
+      onChange={handleSelectedBoard}
+      // Render the input tag to enter search content
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label='Type to search...'
+          size='small'
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position='start'>
+                <SearchIcon sx={{ color: "white" }} />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <>
+                {loading ? (
+                  <CircularProgress sx={{ color: "white" }} size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+          sx={{
+            "& label": { color: "white" },
+            "& input": { color: "white" },
+            "& label.Mui-focused": { color: "white" },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": { borderColor: "white" },
+              "&:hover fieldset": { borderColor: "white" },
+              "&.Mui-focused fieldset": { borderColor: "white" },
+            },
+            ".MuiSvgIcon-root": { color: "white" },
+          }}
+        />
+      )}
+    />
+  );
+}
+
+export default AutoCompleteSearchBoard;
